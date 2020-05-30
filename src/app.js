b9badce80a9c2cd9262c4a75b83ae6a13e520942
zuolung@126.com <apple@AppledeMacBook-Pro.local>
2020-05-30 17:36:31 +0800

    init

---
+++ src/app.js
@@ -0,0 +1,43 @@
+import React from "react";
+import pressImg from "./index";
+
+export default class Demo extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  handleChange = () => {
+    console.log(this.refs)
+    pressImg({
+      file: this.refs["fileImg"].files[0],
+      succ: (resultFile, compressInfo) => {
+        //如果不是null就是压缩成功
+        if (resultFile) {
+          //TODO
+          console.log(resultFile, compressInfo)
+          document.querySelector("#showImage").src = resultFile.base64;
+          this.setState({
+            compressInfo
+          })
+        }
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="file"
+          ref="fileImg"
+          className="fileImg"
+          style={{ outline: "none" }}
+          onChange={this.handleChange}
+        />
+        <img src="" id="showImage" style={{ width: 400 }} />
+        <div style={{ margin: 50, color: "green" }}>{this.state.compressInfo}</div>
+      </div>
+    )
+  }
+}
